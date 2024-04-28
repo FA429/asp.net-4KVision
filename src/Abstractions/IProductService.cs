40b25d360632aba5c61b8e0a459aa893278ddf37
AZ-007 <aboodi-75-@hotmail.com>
2024-04-28 16:53:14 +0300

    push uesr service

---
+++ src/Abstractions/IProductService.cs
@@ -0,0 +1,9 @@
+using sda_onsite_2_csharp_backend_teamwork.src.Entities;
+
+namespace sda_onsite_2_csharp_backend_teamwork.src.Abstractions
+{
+    public interface IProductService
+    {
+        public List<Product> FindAll();
+    }
+}
--- src/Services/ProdcutService.cs
+++ src/Services/ProdcutService.cs
@@ -1,7 +1,14 @@
+using sda_onsite_2_csharp_backend_teamwork.src.Abstractions;
+using sda_onsite_2_csharp_backend_teamwork.src.Entities;
+
 namespace sda_onsite_2_csharp_backend_teamwork.src.Services
 {
-    public class ProdcutService
+    public class ProdcutService : IProductService
     {
-        
+        public List<Product> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
