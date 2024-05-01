4773b2ea1a6842fe7da0951090f9e387a5a581dd
fakhraldeen18 <fakhraldeen18@gmail.com>
2024-05-01 09:47:58 +0300

    add packges and create config

--- src/Databases/DatabaseContext.cs
+++ src/Databases/DatabaseContext.cs
@@ -1,28 +1,28 @@
 
 
+using Microsoft.EntityFrameworkCore;
 using sda_onsite_2_csharp_backend_teamwork.src.Entities;
 
 namespace sda_onsite_2_csharp_backend_teamwork.src.Databases
 {
-    public class DatabaseContext
+    public class DatabaseContext : DbContext
     {
         public List<Order> Order { get; set; }
-        public List<User> Users { get; set; }
+        public DbSet<User> Users { get; set; }
         public List<Product> Products { get; set; }
         public List<OrderItem> Order_Item { get; set; }
         public List<Category> categories;
+        private IConfiguration _config;
 
-        public DatabaseContext()
+        public DatabaseContext(IConfiguration config)
         {
+            _config = config;
             Order = [
                 new Order ("1","11"),
                 new Order ("2","22"),
             ];
 
-            Users = [
-            new User("1","user","Faisal","aa123","user23@gmail.com","0569113170"),
-            new User("2","user","Fakhrdeen","ff123","user23@gmail.com","0501121033"),
-        ];
+
             Products = [
             new Product("1","23","Iphone", "2000"),
             new Product("2","24","MacBook", "5000"),
@@ -39,6 +39,9 @@ namespace sda_onsite_2_csharp_backend_teamwork.src.Databases
             new Category("3", "shoes")
                 ];
         }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql(@$"Host={_config["Db:Host"]};Username={_config["Db:Username"]};Password={_config["Db:Password"]};Database={_config["Db:Database"]}");
 
     }
 }
