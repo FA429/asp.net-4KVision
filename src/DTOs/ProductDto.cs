e590f0958623883e39d37abac793774d2a98d6e1
faisal al-anezi <fa.429@hotmail.com>
2024-05-30 10:17:29 +0300

    update the dateBasa

--- src/DTOs/ProductDto.cs
+++ src/DTOs/ProductDto.cs
@@ -32,11 +32,9 @@ namespace sda_onsite_2_csharp_backend_teamwork.src.DTOs
     }
     public class ProductUpdateDto
     {
-        public string? Name { get; set; }
+        public string Name { get; set; }
         public int Price { get; set; }
         public string? Image { get; set; }
-        public string? Background { get; set; }
-
         public string Description { get; set; }
         public int Quantity { get; set; }
 
